33bc1acea2fa964d7a30dd79a6fb808b1f5ca23e
Cirelly <cirellybryan@gmail.com>
2021-05-04 11:10:52 -0400

    Added courses endpoints crud

--- controllers/course.controller.js
+++ controllers/course.controller.js
@@ -18,8 +18,50 @@ const Course = require('../models/course.models')
     })
 } 
 
+const getCourses = (req, res) => {
+    Course.find()
+        .sort({order: "asc"})
+        .exec((err, courseStored)=> {
+            if(err){
+                res.status(500).send({code: 500, message: "Server Error."})
+            }else if(!courseStored){
+                res.status(404).send({code: 404, message: "Course is not found"})
+            }else {
+                res.status(200).send({code: 200, courses: courseStored})
+            }
+        })   
+}
+const deleteCourse = (req, res) => {
+    const {id} = req.params;
+    Course.findByIdAndRemove(id, (err, deletedCourse) => {
+        if(err){
+            res.status(500).send({code: 500, message: "Server Error."})
+        }else if(!deletedCourse){
+            res.status(404).send({code: 404, message: "Course not found."})
+        }else{
+            res.status(200).send({code:200, message:"Course deleted successfully."})
+        }
+    })
+}
+
+const updateCourse = (req, res) => {
+    let courseData = req.body;
+    const params = req.params;
+    Course.findByIdAndUpdate(params.id, courseData, (err, courseUpdated)=> {
+        if(err){
+            res.status(500).send({code: 500, message:"Server Error."})
+        } else if(!courseUpdated){
+            res.status(404).send({code: 404, message: "Course not found."})
+        }else {
+            res.status(200).send({code: 200, message:"Course updated successfully."})
+        }
+    })
+}
 
 module.exports = {
     addCourse,
+    getCourses,
+    deleteCourse,
+    updateCourse,
 
 }
--- routes/course.routes.js
+++ routes/course.routes.js
@@ -7,6 +7,8 @@ const md_auth = require('../ middleware/authenticated')
 const api = express.Router();
 
 api.post('/add-course', [md_auth.ensureAuth], CourseController.addCourse);
-
+api.get('/get-courses', CourseController.getCourses);
+api.delete('/delete-course/:id', [md_auth.ensureAuth], CourseController.deleteCourse);
+api.put('/update-course/:id', [md_auth.ensureAuth], CourseController.updateCourse);
 
 module.exports=api;
