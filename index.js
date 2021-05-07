19fbc48560719a521eb8d7a82c80d1e5437717f0
Cirelly <cirellybryan@gmail.com>
2021-05-07 13:25:28 -0400

    minor fix

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ require('./database');
 const {port} = require('./config');
 
 const app = require('./app')
-app.listen(port, () =>{
+app.listen(port,'0.0.0.0', () =>{
     console.log("server on port", port);
 } )
